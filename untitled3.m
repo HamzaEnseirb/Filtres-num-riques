fd565c78846ffec1c57e366012ecb4b58d541f04
HamzaEnseirb <56870021+HamzaEnseirb@users.noreply.github.com>
2020-12-17 09:46:56 +0100

    Update untitled3.m

--- untitled3.m
+++ untitled3.m
@@ -1,4 +1,9 @@
 clear, clc, close all;
 
-AR = autoregressif(2,4,5000);
-plot(AR)
+[AR,C] = autoregressif(2,4,5000);
+N = 5000;
+f = -1/2:1/N:1/2-1/N;
+ecart = 1;
+H = freqz(1, C, 2*pi*f);
+figure, plot(f,(abs(fftshift(fft(AR))).^2)/N);
+hold on, plot(f,(abs(H).^2)*ecart);
